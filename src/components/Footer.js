7e558b7eaeda521ad364c87ad0d184dad211b7ae
MattBarnabo <mattbarnabo@gmail.com>
2024-04-12 16:49:51 -0500

    adds changes to footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,24 +3,24 @@ import Logo from "../assets/Footerlogo.png"
 import { NavLink } from "react-router-dom"
 
 const Footer = () => {
-  return (
-    <div className="footer-bar">
-      <div>
-        <p className="footer-text">
-          &copy; Matt|Amir|Louie LEARN Academy Alpha 2024
-        </p>
-      </div>
-      <div>
-        <NavLink to="/">
-          <img
-            className="footer-logo"
-            src={Logo}
-            alt="roof tops with sun behind it"
-          />
-        </NavLink>
-      </div>
-    </div>
-  )
+	return (
+		<div className="footer-bar">
+			<div>
+				<p className="footer-text">
+					&copy; Matt | Amir | Louie | LEARN Academy Alpha 2024
+				</p>
+			</div>
+			<div>
+				<NavLink to="/">
+					<img
+						className="footer-logo"
+						src={Logo}
+						alt="roof tops with sun behind it"
+					/>
+				</NavLink>
+			</div>
+		</div>
+	)
 }
 
 export default Footer
