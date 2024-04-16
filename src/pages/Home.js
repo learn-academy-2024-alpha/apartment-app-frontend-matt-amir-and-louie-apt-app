5d1da7e341df4a0f540d8cf02d9fdbb4f9a281ee
MattBarnabo <mattbarnabo@gmail.com>
2024-04-16 14:01:35 -0500

    adds searchbar to homepage, sign up and logout buttons to header,  removes search bar from header, updates test for search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,22 @@
 import React from "react"
-import apartment5 from "../assets/apartments/apartment5.avif"
 import Slider from "../components/Slider"
+import SearchBar from "../components/SearchBar"
 
 const Home = ({ apartments }) => {
 	return (
 		<div>
-			<div className="home-img-container">
-				<img
-					className="home-image"
-					src={apartment5}
-					alt="brown apartment buildings"
-				/>
+			<div>
+				<div className="home-image">
+					<div className="home-image-text">
+						<h2 className="home-text-h2">Find Your New Flat</h2>
+						<h3 className="home-text-h3">
+							Rent, Buy, Sell in just a few clicks
+							<div className="home-search-bar-container">
+								<SearchBar />
+							</div>
+						</h3>
+					</div>
+				</div>
 			</div>
 			<Slider>
 				{apartments.map(
