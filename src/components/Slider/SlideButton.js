de88f3ae87fca2ade477192393edd6ac4b0fd3aa
MattBarnabo <mattbarnabo@gmail.com>
2024-04-16 14:03:51 -0500

    adds partial functionality of slider component

--- src/components/Slider/SlideButton.js
+++ src/components/Slider/SlideButton.js
@@ -2,12 +2,12 @@ import React from "react"
 import IconArrowDown from "./../Icons/IconArrowDown"
 import "./SlideButton.css"
 
-const SlideButton = ({ onClick, type }) => (
-  <button className={`slide-button slide-button--${type}`} onClick={onClick}>
-    <span>
-      <IconArrowDown />
-    </span>
-  </button>
+const SlideButton = ({ onClick }) => (
+	<button className="slide-button slide-button-prev" onClick={onClick}>
+		<span>
+			<IconArrowDown />
+		</span>
+	</button>
 )
 
 export default SlideButton
