9203d2899fb3e30ac497ac57b8fd778fd271a833
MattBarnabo <mattbarnabo@gmail.com>
2024-04-17 11:27:03 -0500

    adds return changes text tag size and order, adds styling for image and content fields

--- src/components/Slider/Content.js
+++ src/components/Slider/Content.js
@@ -2,36 +2,39 @@ import React from "react"
 import IconCross from "./../Icons/IconCross"
 import "./Content.css"
 
-const Content = ({ apartment, onClose }) => (
-	<div className="content">
-		<div className="content-background">
-			<div className="content-background-shadow" />
-			<div
-				className="content-background-image"
-				style={{ backgroundImage: `url(${apartment.image})` }}
-			/>
-		</div>
-		<div className="content-area">
-			<div className="content-area-container">
-				<div className="content-title">{apartment.street}</div>
-				<div className="content-description">
-					<h4>
-						{apartment.street},{apartment.unit},{apartment.city},
-						{apartment.state}
-					</h4>
-					<h4>{apartment.square_footage}</h4>
-					<h4>{apartment.price}</h4>
-					<h4>{apartment.bedrooms}</h4>
-					<h4>{apartment.bathrooms}</h4>
-					<h4>{apartment.pets}</h4>
-					<h4>{apartment.pets}</h4>
-				</div>
+const Content = ({ apartment, onClose }) => {
+	return (
+		<div className="content">
+			<div className="content-background">
+				<div className="content-background-shadow" />
+				<div
+					className="content-background-image"
+					style={{ backgroundImage: `url(${apartment.image})` }}
+				/>
 			</div>
-			<button className="content-close" onClick={onClose}>
-				<IconCross />
-			</button>
+			<div className="content-area">
+				<div className="content-area-container">
+					<div className="content-title">{apartment.street}</div>
+					<div className="content-description">
+						<h5>
+							{apartment.unit} {apartment.street},
+						</h5>
+						<h5>
+							{apartment.city}, {apartment.state}
+						</h5>
+						<h5>{apartment.square_footage} sq.ft.</h5>
+						<h5>£{apartment.price}</h5>
+						<h5>
+							{apartment.bedrooms} bed | {apartment.bathrooms} bath
+						</h5>
+						<h5> Pets allowed: {apartment.pets}</h5>
+					</div>
+				</div>
+				<button className="content-close" onClick={onClose}>
+					<IconCross />
+				</button>
+			</div>
 		</div>
-	</div>
-)
-
+	)
+}
 export default Content
