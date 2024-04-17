e41adaa23036e7a2da93719c94926316701ecab6
MattBarnabo <mattbarnabo@gmail.com>
2024-04-17 17:10:32 -0500

    adds button functionality for logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,46 @@
 import React from "react"
 import { Nav } from "reactstrap"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import Logo from "../assets/logos/FlatFinderLogo.png"
 
-const Header = () => {
-  return (
-    <Nav className="me-auto">
-      <div className="header-navlink-left">
-        <NavLink className="navlink" to="/index">
-          Rent
-        </NavLink>
-        <NavLink className="navlink">List</NavLink>
-        <NavLink className="navlink">Manage</NavLink>
-      </div>
-      <div className="logo-container">
-        <NavLink to="/">
-          <img
-            className="header-logo-center"
-            src={Logo}
-            alt="flat finders logo"
-          />
-        </NavLink>
-      </div>
-      <div className="header-navlink-right">
-        <NavLink className="navlink" to="/signup">
-          Sign Up
-        </NavLink>
-        <NavLink className="navlink" to="/signin">
-          Sign In
-        </NavLink>
-        <NavLink className="navlink" to="/">
-          Logout
-        </NavLink>
-      </div>
-    </Nav>
-  )
+const Header = ({ logOut }) => {
+	const navigate = useNavigate()
+
+	const handleLogOut = () => {
+		logOut()
+		navigate("/")
+	}
+	return (
+		<Nav className="me-auto">
+			<div className="header-navlink-left">
+				<NavLink className="navlink" to="/index">
+					Rent
+				</NavLink>
+				<NavLink className="navlink">List</NavLink>
+				<NavLink className="navlink">Manage</NavLink>
+			</div>
+			<div className="logo-container">
+				<NavLink to="/">
+					<img
+						className="header-logo-center"
+						src={Logo}
+						alt="flat finders logo"
+					/>
+				</NavLink>
+			</div>
+			<div className="header-navlink-right">
+				<NavLink className="navlink" to="/signup">
+					Sign Up
+				</NavLink>
+				<NavLink className="navlink" to="/signin">
+					Sign In
+				</NavLink>
+				<NavLink className="navlink" to="/" onClick={handleLogOut}>
+					Logout
+				</NavLink>
+			</div>
+		</Nav>
+	)
 }
 
 export default Header
