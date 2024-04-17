1383ca7f31c3bdd14ef84eef533abda813c9c2a1
Rashadjaxon <amirjacksonscholar@gmail.com>
2024-04-17 12:01:14 -0700

    💪added sign-in and sign-up functionality

--- src/App.js
+++ src/App.js
@@ -9,26 +9,75 @@ import mockUsers from "./mockUsers.js"
 import mockApartments from "./mockApartments.js"
 import "./App.css"
 import NotFound from "./pages/NotFound.js"
+import SignIn from "./pages/SignIn.js"
+import SignUp from "./pages/SignUp.js"
 
 const App = () => {
-	const [currentUser, setCurrentUser] = useState(mockUsers[0])
-	const [apartments, setApartments] = useState(mockApartments)
+  const [currentUser, setCurrentUser] = useState(mockUsers[0])
+  const [apartments, setApartments] = useState(mockApartments)
 
-	return (
-		<>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home apartments={apartments} />} />
-				<Route path="*" element={<NotFound />} />
-				<Route path="/index" element={<Index apartments={apartments} />} />
-				<Route
-					path="/apartment/:id"
-					element={<Show apartments={apartments} />}
-				/>
-			</Routes>
-			<Footer />
-		</>
-	)
+  const signIn = async (user) => {
+    try {
+      const signInResponse = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+      if (!signInResponse) {
+        throw new Error(signInResponse.errors)
+      }
+      const payload = await signInResponse.json()
+      localStorage.setItem("token", signInResponse.headers.get("Authorization"))
+      localStorage.setItem("user", JSON.stringify(payload))
+      setUser(payload)
+    } catch (error) {
+      console.error("Error fetching user sign in request")
+    }
+  }
+
+  const signUp = async (user) => {
+    try {
+      const signUpResponse = await fetch("http://localhost:3000/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+      if (!signUpResponse) {
+        throw new Error(signUpResponse.errors)
+      }
+      const payload = await signUpResponse.json()
+      localStorage.setItem("token", signUpResponse.headers.get("Authorization"))
+      localStorage.setItem("user", JSON.stringify(payload))
+      setUser(payload)
+    } catch (error) {
+      console.error("Error fetching user sign up request")
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home apartments={apartments} />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/index" element={<Index apartments={apartments} />} />
+        <Route
+          path="/apartment/:id"
+          element={<Show apartments={apartments} />}
+        />
+        <Route path="/signin" element={<SignIn signIn={signIn} />} />
+        <Route path="/signup" element={<SignUp signUp={signUp} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 
 export default App
