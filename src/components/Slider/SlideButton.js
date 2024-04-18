345fd82591a28745f9b91c9e102b505f76bb3422
Louie-cpu <luis.u.m779@gmail.com>
2024-04-18 10:40:42 -0700

    code cleaned up on slider button

--- src/components/Slider/SlideButton.js
+++ src/components/Slider/SlideButton.js
@@ -5,33 +5,28 @@ import "./SlideButtonLeft.css"
 import classNames from "classnames"
 
 const SlideButton = ({ onClick }) => {
-	const [isClicked, setIsClicked] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
 
-	// useEffect(() => {
-	// 	const checkForClick = localStorage.getItem("isClicked")
-	// 	if (checkForClick) setIsClicked(JSON.parse(checkForClick))
-	// }, [])
-
-	const handleToggle = () => {
-		setIsClicked(!isClicked)
-	}
-	const conditionalStyles = classNames("SlideButton", {
-		"slide-button-left": isClicked,
-		"slide-button-right": !isClicked,
-	})
-	return (
-		<button
-			className={conditionalStyles}
-			onClick={() => {
-				handleToggle()
-				onClick()
-			}}
-		>
-			<span>
-				<IconArrowDown />
-			</span>
-		</button>
-	)
+  const handleToggle = () => {
+    setIsClicked(!isClicked)
+  }
+  const conditionalStyles = classNames("SlideButton", {
+    "slide-button-left": isClicked,
+    "slide-button-right": !isClicked,
+  })
+  return (
+    <button
+      className={conditionalStyles}
+      onClick={() => {
+        handleToggle()
+        onClick()
+      }}
+    >
+      <span>
+        <IconArrowDown />
+      </span>
+    </button>
+  )
 }
 
 export default SlideButton
