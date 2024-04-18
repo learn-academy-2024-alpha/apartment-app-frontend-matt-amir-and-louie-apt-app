659bc2aaf87d1ae10c87115d40d612367fee299e
Louie-cpu <luis.u.m779@gmail.com>
2024-04-18 11:42:36 -0700

    passing off edit and new files

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,50 +5,52 @@ import Logo from "../assets/logos/FlatFinderLogo.png"
 import NavButton from "./NavButton"
 
 const Header = ({ logOut, user }) => {
-	const navigate = useNavigate()
+  const navigate = useNavigate()
 
-	const handleLogOut = () => {
-		logOut()
-		navigate("/")
-	}
-	return (
-		<Nav className="me-auto">
-			<div className="header-navlink-left">
-				<NavLink className="navlink" to="/index">
-					Rent
-				</NavLink>
-				<NavLink className="navlink">List</NavLink>
-				<NavLink className="navlink">Manage</NavLink>
-			</div>
-			<div className="logo-container">
-				<NavLink to="/">
-					<img
-						className="header-logo-center"
-						src={Logo}
-						alt="flat finders logo"
-					/>
-				</NavLink>
-			</div>
-			<div className="header-navlink-right">
-				{user && (
-					<NavLink to="/MyFlats" className="navlink">
-						My Flats
-					</NavLink>
-				)}
-				{!user && (
-					<NavLink to="/signin" className="navlink">
-						Sign In
-					</NavLink>
-				)}
-				{!user && <NavButton url="/signup" buttonContent="Sign Up" />}
-				{user && (
-					<button className="nav-button" onClick={handleLogOut}>
-						Log Out
-					</button>
-				)}
-			</div>
-		</Nav>
-	)
+  const handleLogOut = () => {
+    logOut()
+    navigate("/")
+  }
+  return (
+    <Nav className="me-auto">
+      <div className="header-navlink-left">
+        <NavLink className="navlink" to="/index">
+          Rent
+        </NavLink>
+        <NavLink className="navlink" to="/new">
+          List
+        </NavLink>
+        <NavLink className="navlink">Manage</NavLink>
+      </div>
+      <div className="logo-container">
+        <NavLink to="/">
+          <img
+            className="header-logo-center"
+            src={Logo}
+            alt="flat finders logo"
+          />
+        </NavLink>
+      </div>
+      <div className="header-navlink-right">
+        {user && (
+          <NavLink to="/MyFlats" className="navlink">
+            My Flats
+          </NavLink>
+        )}
+        {!user && (
+          <NavLink to="/signin" className="navlink">
+            Sign In
+          </NavLink>
+        )}
+        {!user && <NavButton url="/signup" buttonContent="Sign Up" />}
+        {user && (
+          <button className="nav-button" onClick={handleLogOut}>
+            Log Out
+          </button>
+        )}
+      </div>
+    </Nav>
+  )
 }
 
 export default Header
