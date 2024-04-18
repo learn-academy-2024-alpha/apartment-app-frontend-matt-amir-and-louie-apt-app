a1a337f567cf373de617d5ae8b1de1fdf298e276
MattBarnabo <mattbarnabo@gmail.com>
2024-04-18 11:33:08 -0500

    adds some function to the slider button

--- src/components/Slider/SlideButton.js
+++ src/components/Slider/SlideButton.js
@@ -1,10 +1,32 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import IconArrowDown from "./../Icons/IconArrowDown"
-import "./SlideButton.css"
+import "./SlideButtonRight.css"
+import "./SlideButtonLeft.css"
+import classNames from "classnames"
 
 const SlideButton = ({ onClick }) => {
+	const [isClicked, setIsClicked] = useState(false)
+
+	// useEffect(() => {
+	// 	const checkForClick = localStorage.getItem("isClicked")
+	// 	if (checkForClick) setIsClicked(JSON.parse(checkForClick))
+	// }, [])
+
+	const handleToggle = () => {
+		setIsClicked(!isClicked)
+	}
+	const conditionalStyles = classNames("SlideButton", {
+		"slide-button-left": isClicked,
+		"slide-button-right": !isClicked,
+	})
 	return (
-		<button className="slide-button" onClick={onClick}>
+		<button
+			className={conditionalStyles}
+			onClick={() => {
+				handleToggle()
+				onClick()
+			}}
+		>
 			<span>
 				<IconArrowDown />
 			</span>
